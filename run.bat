31374691f95b2c28e9911e6bdc4324ca7a4488d5
udream/1 <wjzb@example.com>
2025-07-02 14:21:56 +0800

    得先切换到 udt_tianfeng 再 uv run 否则 vnpy 内部获取得到的文件路径会出问题，导致加载不到策略文件 .py

--- run.bat
+++ run.bat
@@ -1,7 +1,9 @@
 @echo off
 echo Starting main.py using uv...
 
-uv run udt_tianfeng\main.py --upgrade-package akshare
+cd udt_tianfeng
+
+uv run main.py --upgrade-package akshare
 
 if %ERRORLEVEL% NEQ 0 (
     echo Error occurred while running main.py
